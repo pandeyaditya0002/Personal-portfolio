6cf2a5585ccc93688ce9f38a251a67954b9ee33f
pandeyaditya0002 <103255068+pandeyaditya0002@users.noreply.github.com>
2024-02-11 15:20:57 +0530

    banner update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -18,7 +18,7 @@ const Banner: React.FC<{}> = () => {
         </div>
 
         <div className="flex flex-col gap-6 mt-6 cursor-pointer animate-bounce tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
-          Ibrahim Memon!
+          Aditya
         </div>
         <p className="text-2xl font-medium tracking-tighter  text-gray-300 max-w-[600px]">
           I do Code &{" "}
