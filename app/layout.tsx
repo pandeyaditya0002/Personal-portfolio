06a3a98d76c0a709f115f56c426ff779fd41b620
pandeyaditya0002 <103255068+pandeyaditya0002@users.noreply.github.com>
2024-02-11 14:50:05 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarsBackground";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    "https://developer-portfolio-ibrahim-memons-projects.vercel.app"
+  ),
+  title: "Ibrahim Portfolio",
+  description: "Developer Portfolio By Ibrahim Memon",
+  keywords: ["Developer", "Portfolio", "Developer Portflio", "Ibrahim Memon"],
+  openGraph: {
+    title: "Ibrahim Memon",
+    description: "Software Engineer",
+    images: "/OpenGraph.jpg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#111] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Banner.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+const Banner: React.FC<{}> = () => {
+  return (
+    <div className="flex flex-row items-center justify-center px-20 mt-[150px] z-[20] ">
+      <div className="flex flex-col  justify-center  text-center">
+        <div className="justify-center flex ">
+          <Image
+            priority
+            src="/Me.png"
+            height={300}
+            width={300}
+            alt="Ibrahim Memon"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 mt-6 cursor-pointer animate-bounce tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
+          Ibrahim Memon!
+        </div>
+        <p className="text-2xl font-medium tracking-tighter  text-gray-300 max-w-[600px]">
+          I do Code &{" "}
+          <span className="text-transparent font-semibold  bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
+            Chill
+          </span>{" "}
+          🍿
+        </p>
+        <p className="text-md text-gray-200 my-5 max-w-[600px]">
+          Passionate Software Engineer with a focus on React Native development,
+          dedicated to crafting elegant and user-friendly mobile applications.
+        </p>
+        <div className="text-md flex justify-center">
+          <button
+            onClick={() => window.open("mailto:ibrahimmemon930@gmail.com")}
+            className=" z-[1]     padding-20  hover:bg-white rounded-3xl  text-white font-semibold hover:text-black py-3 px-10  border-[0.1px] border-white hover:border-transparent "
+          >
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
