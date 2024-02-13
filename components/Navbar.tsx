fb485a7a7a73e2e155cefe3131cb07cd6131b3c8
pandeyaditya0002 <103255068+pandeyaditya0002@users.noreply.github.com>
2024-02-13 14:52:17 +0530

    final Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+
 import React from "react";
 
 const Navbar: React.FC<{}> = () => {
@@ -8,14 +8,7 @@ const Navbar: React.FC<{}> = () => {
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a href="/" className="h-auto w-auto flex flex-row items-center">
-          <Image
-            src="/Logo.svg"
-            alt="logo"
-            width={100}
-            height={100}
-            sizes="100vw"
-            className="w-full h-auto"
-          />
+         
         </a>
 
         <div className="flex flex-row gap-5">
